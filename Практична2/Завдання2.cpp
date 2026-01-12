83cfafec134557d58b8d73841f2713ff5d5c95ad
h8nix <suhickijs@gmail.com>
2026-01-13 00:26:25 +0200

    Update code

---
+++ Практична2/Завдання2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, a, b;
+
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Remainder of " << n << " / 7 is: " << n % 7 << endl;
+
+    cout << "\nEnter two numbers: ";
+    cin >> a >> b;
+
+    if (a % b == 0) {
+        cout << a << " is divisible by " << b << " without remainder." << endl;
+    } else {
+        cout << a << " is NOT divisible by " << b << ". Remainder: " << a % b << endl;
+    }
+
+    // Перевірка першого числа
+    if (a % 2 == 0) {
+        cout << "Number " << a << " is even." << endl;
+    } else {
+        cout << "Number " << a << " is odd." << endl;
+    }
+
+    if (b % 2 == 0) {
+        cout << "Number " << b << " is even." << endl;
+    } else {
+        cout << "Number " << b << " is odd." << endl;
+    }
+
+    return 0;
+}
